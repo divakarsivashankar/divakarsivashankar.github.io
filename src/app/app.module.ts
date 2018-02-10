3d2e410af83f97710c009e18450864846d8129c5
sakthiveldhanasekaran <sakthiveldhanasekaran@live.com>
2018-02-10 17:13:56 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContainerComponent } from './container/container.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CareerComponent } from './career/career.component';
 
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'projects',
+    component: ProjectsComponent
+  },
+  {
+    path:'career',
+    component: CareerComponent
+  },
+  { 
+    path: '**',
+    component: PageNotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContainerComponent,
+    AboutComponent,
+    ProjectsComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    CareerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
